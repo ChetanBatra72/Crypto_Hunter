1cf950f8aff482de758f76333f95c2df0a2af1ac
ChetanBatra72 <Chetanbat2001@gmail.com>
2024-02-05 23:55:42 +0530

    Firebase Integrated

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBk9Yw3ZFdkU-S6donnjac58H1YCbC7tpY",
+    authDomain: "crypto-hunter-4e1e7.firebaseapp.com",
+    projectId: "crypto-hunter-4e1e7",
+    storageBucket: "crypto-hunter-4e1e7.appspot.com",
+    messagingSenderId: "953287182938",
+    appId: "1:953287182938:web:99bfcce73151045b052663"
+  };
+  export default firebaseConfig;
